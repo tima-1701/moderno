d38a4d16cbeea099705e9f42071a96aa2923a8dd
tima-1701 <tima-1701@mail.ru>
2021-03-29 18:57:30 +0600

    name

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-const { on } = require('gulp');
+// const { on } = require('gulp');
 
 $(function(){
 
@@ -19,6 +19,11 @@ $(function(){
       $('.menu__btn').on('click', function(){
         $('.menu__list').slideToggle();
       });
+      
+      $('.header__btn-menu').on('click', function(){
+        $('.header__box').toggleClass('active');
+      });
+
 
     var mixer = mixitup('.products__inner-box');
 
